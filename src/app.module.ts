0624f27a6269aac679534bf0a659dcbdbc4ce7c0
MatthiasWanner <matthias.wanner@gmail.com>
2022-03-09 00:02:39 +0100

    :package: (ws) Install ws deps + enable graphql-ws in app module
    Install graphql-subscriptions + @types/graphql.

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,8 @@ import { ConfigModule } from '@nestjs/config';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { AppResolver } from './app.resolver';
 import { AppService } from './app.service';
+import { PartyModule } from './party/party.module';
+import { AppController } from './app.controller';
 import configuration from './configuration';
 
 @Module({
@@ -13,13 +15,18 @@ import configuration from './configuration';
       debug: true,
       playground: true,
       autoSchemaFile: true,
+      subscriptions: {
+        'graphql-ws': true,
+      },
     }),
     ConfigModule.forRoot({
       envFilePath: ['.env.local', '.env'],
       isGlobal: true,
       load: [configuration],
     }),
+    PartyModule,
   ],
   providers: [AppResolver, AppService],
+  controllers: [AppController],
 })
 export class AppModule {}
