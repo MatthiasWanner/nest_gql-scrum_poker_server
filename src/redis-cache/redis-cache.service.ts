2d6852922819ecae2593eb7129dfea7f3f069f90
MatthiasWanner <matthias.wanner@gmail.com>
2022-03-09 22:31:20 +0100

    :label: (redis) Typing redis services

--- src/redis-cache/redis-cache.service.ts
+++ src/redis-cache/redis-cache.service.ts
@@ -1,23 +1,24 @@
 import { CACHE_MANAGER, Inject, Injectable } from '@nestjs/common';
 import { Cache } from 'cache-manager';
+import { RedisCacheResponse } from './interfaces/redis.interfaces';
 
 @Injectable()
 export class RedisCacheService {
   constructor(@Inject(CACHE_MANAGER) private readonly cache: Cache) {}
 
-  async get(key): Promise<any> {
+  async get<T>(key: string): Promise<T> {
     return await this.cache.get(key);
   }
 
-  async set(key, value) {
-    await this.cache.set(key, value, 1000);
+  async set<T>(key: string, value: T): Promise<RedisCacheResponse> {
+    return await this.cache.set(key, value, 1000);
   }
 
   async reset() {
     await this.cache.reset();
   }
 
-  async del(key) {
+  async del(key: string) {
     await this.cache.del(key);
   }
 }
