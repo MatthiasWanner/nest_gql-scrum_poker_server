11c65bf2d23855b6c96d934aec62311462599509
MatthiasWanner <matthias.wanner@gmail.com>
2022-03-11 16:43:02 +0100

    :sparkles: (auth) Implement JWT services in aut module to enable sessions

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,8 +1,22 @@
 import { Module } from '@nestjs/common';
 import { UuidService } from './uuid.service';
+import { AuthService } from './auth.service';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
-  providers: [UuidService],
-  exports: [UuidService],
+  imports: [
+    JwtModule.registerAsync({
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        secret: configService.get('JWT_SECRET'),
+        signOptions: {
+          expiresIn: configService.get('JWT_EXPIRES_IN') || 3600,
+        },
+      }),
+    }),
+  ],
+  providers: [UuidService, AuthService],
+  exports: [UuidService, AuthService],
 })
 export class AuthModule {}
