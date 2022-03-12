9aadbe209434d3999fdd8cf00c09202d01833932
MatthiasWanner <matthias.wanner@gmail.com>
2022-03-12 22:09:29 +0100

    :label: (types) Manage RedisCacheService method's responses

--- src/redis-cache/interfaces/redis.interfaces.ts
+++ src/redis-cache/interfaces/redis.interfaces.ts
@@ -1,6 +1,5 @@
 /**
- * @description Redis cache response
- * @description In reality defined status 'OK' for example
- * @description But I have to list all status before typing
+ * @type RedisCacheResponse cache response
+ * @description Manage responses for RedisCacheService 'set' method
  */
-export type RedisCacheResponse = string;
+export type RedisCacheResponse = 'OK' | 'FAILED';
