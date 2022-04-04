a72a381b783eaab68d55e2c96622c227fca184e4
MatthiasWanner <matthias.wanner@gmail.com>
2022-04-04 20:41:41 +0200

    :package: (deps) install and use cookie parser

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { NestFactory } from '@nestjs/core';
+import * as cookieParser from 'cookie-parser';
 import { AppModule } from './app.module';
 import config from './configuration';
 
 async function bootstrap() {
   const port = config().port;
   const app = await NestFactory.create(AppModule);
+  app.use(cookieParser());
   await app.listen(port);
 }
 bootstrap();
