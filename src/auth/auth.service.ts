71c60504df4526780016b73fe48a5f82969cfa19
MatthiasWanner <matthias.wanner@gmail.com>
2022-05-01 16:19:48 +0200

    :recycle: (session) Delete jwt iat and exp keys from req.user object

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
-import { LoginArgs, Login } from './models';
+import { UserInSession } from 'src/user/models';
+import { LoginArgs, Login, JwtSession } from './models';
 
 @Injectable()
 export class AuthService {
@@ -13,8 +14,11 @@ export class AuthService {
     };
   }
 
-  verifySessionToken(jwt: string): LoginArgs {
-    return this.jwtService.verify<LoginArgs>(jwt);
+  verifySessionToken(jwt: string): UserInSession {
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    const { exp, iat, ...userSession } =
+      this.jwtService.verify<JwtSession>(jwt);
+    return userSession;
   }
 
   checkRoles(authorizedRoles: string[], userRole: string): boolean {
