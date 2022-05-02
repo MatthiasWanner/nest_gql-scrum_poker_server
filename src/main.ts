04f3003741d52fdd936fd5fceb0c24d6a0646cc0
MatthiasWanner <matthias.wanner@gmail.com>
2022-05-02 21:58:59 +0200

    :recycle: (refactor) Improve bootstrap function logging start sentence

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,10 @@ import { AppModule } from './app.module';
 import config from './configuration';
 
 async function bootstrap() {
-  const port = config().port;
+  const { port } = config();
   const app = await NestFactory.create(AppModule);
   app.use(cookieParser());
   await app.listen(port);
+  console.log(`NestJS server is running on port ${port}`);
 }
 bootstrap();
