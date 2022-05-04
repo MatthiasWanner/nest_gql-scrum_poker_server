a8a9410ea5ac1dc05132b4e1ffc84d1c2cbc7fd4
MatthiasWanner <matthias.wanner@gmail.com>
2022-05-04 15:25:47 +0200

    :bug: (fix) Global app cors config.

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,14 @@ import { AppModule } from './app.module';
 import config from './configuration';
 
 async function bootstrap() {
-  const { port } = config();
+  const { port, corsOrigin } = config();
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    credentials: true,
+    origin: corsOrigin,
+    methods: 'GET,HEAD,POST',
+    allowedHeaders: 'Content-Type, Accept',
+  });
   app.use(cookieParser());
   await app.listen(port);
   console.log(`NestJS server is running on port ${port}`);
