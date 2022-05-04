fda2ed75bb99db7932a0dc53ba6a3c0ea23cbed2
MatthiasWanner <matthias.wanner@gmail.com>
2022-05-04 23:02:16 +0200

    :recycle: (args) Standardize gameId as the reference key to retrieve a game

--- src/game/models/GetGameVotesArgs.ts
+++ src/game/models/GetGameVotesArgs.ts
@@ -3,5 +3,5 @@ import { ArgsType, Field } from '@nestjs/graphql';
 @ArgsType()
 export class GetGameVotesArgs {
   @Field(() => String, { description: 'Game id' })
-  readonly id!: string;
+  readonly gameId!: string;
 }
